a09cb46fb536b1f740ff67b482e5db9a5767b0b6
Artyom <artyommois@024gmail.com>
2023-02-27 19:59:16 +0300

    Fisrst Commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+
+console.log(number);
+
+const obj = {
+	a: 50
+};
+obj.a = 14;
+console.log(obj);
+
+console.log(name);
+var name = 'Ivan';
+
+
+{
+	var result = 50;
+}
+console.log(result);
+
+const vehicleBodyWidth = 4000, 
+	vehicleBodyLength = 5000; 
+console.log('Ширина кузова автомобиля: ' + vehicleBodyWidth + ', длина кузова: ' + vehicleBodyLength);
+
+/* ['Artyom', 'Darya', 'Nikita', 'Valerya'].map(item => ) */
+
+// Request
+/* data 
+response  */
+
+// Типы данных
+console.log(4/0);//Infinity
+console.log('string' * 9);//Nan
+
+
+const arr = ['a', 'b', 'c'];
+const g = 'g';
+const arrObj = {
+	0: 'a',
+	1: 'b',
+	2: 'c'
+};
+
+arrObj.b = '24';
+arrObj['c'] = '18';
+arrObj['g'] = '37';
+console.log(arrObj['b']);
+console.log(arrObj.b);
+console.log(arrObj.c);
+console.log(arrObj.g);
+
+const answers = [];
+
+/* answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', ''); */
+
+console.log(typeof(answers));
+
+const category = 'girls';
+console.log(`https://someurl.com/${category}/6`);
+
+const bro = prompt('Как твое имя, дружище?', '');
+alert(`Привет, ${bro}`);
+
